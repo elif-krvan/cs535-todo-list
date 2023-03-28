8f8061b9633e50817a9a8e5ac632587ae120af03
Elif <elif.kervan@ug.bilkent.edu.tr>
2023-03-28 23:46:11 +0300

    Added create table and insert data sql

--- schema.sql
+++ schema.sql
@@ -26,14 +26,16 @@ VALUES (1, "pass123", "user1", "user1@example.com"),
        (2, "password", "user2", "user2@example.com");
 
 -- Task
-CREATE TABLE IF NOT EXISTS User (
+CREATE TABLE IF NOT EXISTS Task (
     id int PRIMARY KEY AUTO_INCREMENT,
     title varchar(255) NOT NULL,
-    description varchar(1000),
+    description text,
     status varchar(255) NOT NULL,
     deadline datetime NOT NULL,
     creation_time datetime NOT NULL,
-    done_time datetime NOT NULL,
+    done_time datetime,
+    user_id int NOT NULL,
+    task_type varchar(255),
     FOREIGN KEY (user_id)
         REFERENCES User(id)
         ON DELETE CASCADE,
@@ -41,7 +43,14 @@ CREATE TABLE IF NOT EXISTS User (
         REFERENCES TaskType(type)
 );
 
-
-
-
-1 Go for a walk Walk for at least 30 mins Done 2023-03-20 17:00:00 2023-03-15 10:00:00 2023-03-20 10:00:00 1 Health 2 Clean the house Clean the whole house Done 2023-03-18 12:00:00 2023-03-14 09:00:00 2023-03-18 17:00:00 1 Lifestyle 3 Submit report Submit quarterly report Todo 2023-04-12 17:00:00 2023-03-21 13:00:00 null 1 Job 4 Call Mom Call Mom and wish her Todo 2023-04-06 11:00:00 2023-03-23 12:00:00 null 1 Family 5 Gym workout Do weight training for an hour Done 2023-03-19 14:00:00 2023-03-12 10:00:00 2023-03-19 11:00:00 1 Health 6 Play guitar Learn new song for an hour Todo 2023-04-05 20:00:00 2023-03-20 14:00:00 null 2 Hobbies 7 Book flights Book flights for summer vacation Done 2023-03-16 09:00:00 2023-03-13 13:00:00 2023-03-16 11:00:00 2 Lifestyle 8 Write a blog post Write about recent project Todo 2023-04-11 17:00:00 2023-03-22 09:00:00 null 2 Job 9 Grocery shopping Buy groceries for the week Todo 2023-04-05 18:00:00 2023-03-31 10:00:00 null 2 Family 10 Painting Paint a landscape for 2 hours Done 2023-03-23 15:00:00 2023-03-18 14:00:00 2023-03-23 16:00:00 2 Hobbies 
+INSERT INTO Task (id, title, description, status, deadline, creation_time, done_time, user_id, task_type)
+VALUES (1, 'Go for a walk', 'Walk for at least 30 mins', 'Done', '2023-03-20 17:00:00', '2023-03-15 10:00:00', '2023-03-20 10:00:00', 1, 'Health'),
+       (2, 'Clean the house', 'Clean the whole house', 'Done', '2023-03-18 12:00:00', '2023-03-14 09:00:00', '2023-03-18 17:00:00', 1, 'Lifestyle'),
+       (3, 'Submit report', 'Submit quarterly report', 'Todo', '2023-04-12 17:00:00', '2023-03-21 13:00:00', null, 1, 'Job'),
+       (4, 'Call Mom', 'Call Mom and wish her', 'Todo', '2023-04-06 11:00:00', '2023-03-23 12:00:00', null, 1, 'Family'),
+       (5, 'Gym workout', 'Do weight training for an hour', 'Done', '2023-03-19 14:00:00', '2023-03-12 10:00:00', '2023-03-19 11:00:00', 1, 'Health'),
+       (6, 'Play guitar', 'Learn new song for an hour', 'Todo', '2023-04-05 20:00:00', '2023-03-20 14:00:00', null, 2, 'Hobbies'),
+       (7, 'Book flights', 'Book flights for summer vacation', 'Done', '2023-03-16 09:00:00', '2023-03-13 13:00:00', '2023-03-16 11:00:00', 2, 'Lifestyle'),
+       (8, 'Write a blog post', 'Write about recent project', 'Todo', '2023-04-11 17:00:00', '2023-03-22 09:00:00', null, 2, 'Job'),
+       (9, 'Grocery shopping', 'Buy groceries for the week', 'Todo', '2023-04-05 18:00:00', '2023-03-31 10:00:00', null, 2, 'Family'),
+       (10, 'Painting', 'Paint a landscape for 2 hours', 'Done', '2023-03-23 15:00:00', '2023-03-18 14:00:00', '2023-03-23 16:00:00', 2, 'Hobbies');
