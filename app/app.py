c24dc65c08231ef590d02c5a04b22aaf1e68c37a
Elif <elif.kervan@ug.bilkent.edu.tr>
2023-03-29 21:28:11 +0300

    Added create task, delete task and view tasks endpoints

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 
 import re  
 import os
-from app.status import TaskStatus
+from status import TaskStatus
+# from task_table import TaskTable
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
@@ -34,7 +35,7 @@ def login():
             session['username'] = user['username']
             session['email'] = user['email']
             message = 'Logged in successfully!'
-            return redirect(url_for('tasks'))
+            return redirect(url_for('task'))
         else:
             message = 'Please enter correct email / password !'
     return render_template('login.html', message = message)
@@ -66,26 +67,70 @@ def register():
         message = 'Please fill all the fields!'
     return render_template('register.html', message = message)
 
-@app.route('/task', methods =['GET', 'POST'])
-def task():
+@app.route('/task/<int:task_id>', methods =['POST'])
+def delete_task(task_id):
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('SELECT * FROM TaskType')
-    task_types = cursor.fetchall()
-    
-    print(task_types)
-        
+    cursor.execute('DELETE FROM Task WHERE id = %s', (task_id,))
+    mysql.connection.commit()
+    return redirect(url_for('task'))
+
+@app.route('/task', methods =['POST'])
+def create_task():
     if request.method == 'POST' and 'title' in request.form and 'description' in request.form and 'due-date' in request.form and 'task-type' in request.form:
         title = request.form['title']
         description = request.form['description']
         due_date = request.form['due-date']
         task_type = request.form['task-type']
         
-        cursor.execute('INSERT INTO Task (id, title, description, status, deadline, creation_time, done_time, user_id, task_type) VALUES (NULL, % s, % s, %s, % s, curdate(), NULL, session[\'userid\'], %s)', (title, description, TaskStatus.TODO, due_date, task_type))
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('INSERT INTO Task (id, title, description, status, deadline, creation_time, done_time, user_id, task_type) VALUES (NULL, % s, % s, %s, % s, now(), NULL, %s, %s)', (title, description, TaskStatus.TODO.value, due_date, session['userid'], task_type))
         mysql.connection.commit()
         message = 'Task successfully created!'
     elif request.method == 'POST':
         message = 'Please fill all the fields!'
-    return render_template('task.html', task_types = task_types, message = '')
+    return redirect(url_for('task'))
+    # return render_template('task.html', task_types = task_types, task_headers = task_headers, task_todo = task_todo, message = '')
+
+    # cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    # cursor.execute('DELETE FROM Task WHERE id = %s', (task_id,))
+    # mysql.connection.commit()
+    # return "created task bic"
+
+@app.route('/task', methods =['GET', 'POST', 'DELETE'])
+def task():
+    print(request.method)    
+    # get task types from db
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM TaskType')
+    task_types = cursor.fetchall()   
+    
+    # get todo tasks from db
+    cursor.execute('SELECT * FROM Task WHERE status = %s ORDER BY deadline', (TaskStatus.TODO.value,))
+    task_todo = cursor.fetchall()  
+    print("haloo")
+    print(task_todo)
+    
+    task_headers = {
+        'title': 'Title',
+        'description': 'Description',
+        'status': 'Status',
+        'deadline': "Deadline",
+        'creation_time': "Created At",
+        'task_type': 'Category'
+    }
+    
+    if request.method == 'POST' and 'title' in request.form and 'description' in request.form and 'due-date' in request.form and 'task-type' in request.form:
+        title = request.form['title']
+        description = request.form['description']
+        due_date = request.form['due-date']
+        task_type = request.form['task-type']
+        
+        cursor.execute('INSERT INTO Task (id, title, description, status, deadline, creation_time, done_time, user_id, task_type) VALUES (NULL, % s, % s, %s, % s, now(), NULL, %s, %s)', (title, description, TaskStatus.TODO.value, due_date, session['userid'], task_type))
+        mysql.connection.commit()
+        message = 'Task successfully created!'
+    elif request.method == 'POST':
+        message = 'Please fill all the fields!'
+    return render_template('task.html', task_types = task_types, task_headers = task_headers, task_todo = task_todo, message = '')
 
 @app.route('/analysis', methods =['GET', 'POST'])
 def analysis():
---
+++ app/task_table.py
@@ -0,0 +1,6 @@
+# from flask_table import Table, Col
+
+# class TaskTable(Table):
+#     title = Col("title")
+#     description = Col("description")
+#     due_date = Col("due_date")
